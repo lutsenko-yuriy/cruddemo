7836c1ec3a631146f9f3982965b379237b34c1b1
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-10 01:19:47 +0200

    Initial commit

---
+++ src/main/kotlin/com/iurii/cruddemo/CruddemoApplication.kt
@@ -0,0 +1,11 @@
+package com.iurii.cruddemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CruddemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<CruddemoApplication>(*args)
+}
