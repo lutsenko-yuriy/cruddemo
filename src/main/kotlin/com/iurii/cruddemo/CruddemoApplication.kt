944f361321cf24b8fc4f8aa3a00703f520697d25
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-10 01:32:21 +0200

    Add db creds to properties

--- src/main/kotlin/com/iurii/cruddemo/CruddemoApplication.kt
+++ src/main/kotlin/com/iurii/cruddemo/CruddemoApplication.kt
@@ -1,10 +1,15 @@
 package com.iurii.cruddemo
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CruddemoApplication
+class CruddemoApplication: CommandLineRunner {
+    override fun run(vararg args: String?) {
+        println("Hello, world!")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<CruddemoApplication>(*args)
