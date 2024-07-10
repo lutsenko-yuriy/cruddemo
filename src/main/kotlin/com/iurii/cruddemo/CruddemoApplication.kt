0784cd96636b9a9268a4f05814053290e2d6b85f
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-10 20:35:17 +0200

    Initialize db with data

--- src/main/kotlin/com/iurii/cruddemo/CruddemoApplication.kt
+++ src/main/kotlin/com/iurii/cruddemo/CruddemoApplication.kt
@@ -1,13 +1,30 @@
 package com.iurii.cruddemo
 
+import com.iurii.cruddemo.dao.StudentDao
+import com.iurii.cruddemo.entity.Student
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class CruddemoApplication: CommandLineRunner {
-    override fun run(vararg args: String?) {
-        println("Hello, world!")
+class CruddemoApplication {
+
+    @Bean
+    fun commandLineRunner(studentDao: StudentDao): CommandLineRunner {
+        return CommandLineRunner {
+            println("Creating a new student")
+            val student = Student(
+                firstName = "Iurii",
+                lastName = "Lutsenko",
+                email = "iurii@lutsenko.com",
+            )
+
+            println("Adding student to db")
+            studentDao.save(student)
+
+            println("Student saved. Assigned ID is ${student.id}")
+        }
     }
 }
 
