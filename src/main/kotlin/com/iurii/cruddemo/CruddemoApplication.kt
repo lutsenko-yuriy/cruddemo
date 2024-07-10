5435a4e6823e3549d9291bd7334a9ef916f90328
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-10 22:34:36 +0200

    Add ddl-auto property

--- src/main/kotlin/com/iurii/cruddemo/CruddemoApplication.kt
+++ src/main/kotlin/com/iurii/cruddemo/CruddemoApplication.kt
@@ -1,6 +1,7 @@
 package com.iurii.cruddemo
 
 import com.iurii.cruddemo.dao.StudentDao
+import com.iurii.cruddemo.entity.Student
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -12,7 +13,9 @@ class CruddemoApplication {
     @Bean
     fun commandLineRunner(studentDao: StudentDao): CommandLineRunner {
         return CommandLineRunner {
-
+            studentDao.save(
+                Student("Iurii", "Iurii", "iurii@iurii.com")
+            )
         }
     }
 }
